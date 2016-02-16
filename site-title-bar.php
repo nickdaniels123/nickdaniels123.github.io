3ac11e4f2f9e7877c6c953e227aa227d2971a993
Nick Daniels <daniels.nick@gmail.com>
2016-02-16 21:47:53 +0000

    revert to foundation vanilla

--- site-title-bar.php
+++
@@ -1,9 +0,0 @@
-	<div class="medium-8 push-3 columns header">
-		<h1 class="site-title"><a href="index.php">Simon Haugh</a></h1>
-	</div>
-</div>
-
-<div class="row">
-	<div class="medium-8 push-3 columns hr-below-header">
-		<hr/>
-	</div>
