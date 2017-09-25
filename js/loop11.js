5be1c43e2e7b76aa76fbe4324026b27f62404de8
Nick Daniels <daniels.nick@gmail.com>
2017-09-25 10:25:58 +0100

    load js script from separate file

---
+++ js/loop11.js
@@ -0,0 +1,4 @@
+<script type="text/javascript">
+  window.loop11_key = "c28db8607cebb40ebbe9cff35e99aeab56a3f3e7";
+</script>
+<script src="//cdn.loop11.com/my/loop11.js" type="text/javascript" async="async"></script>
