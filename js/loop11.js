b5f817dbf0620b3c3441e5d004b93893a1a1595d
Nick Daniels <daniels.nick@gmail.com>
2017-09-25 11:14:47 +0100

    more tests

--- js/loop11.js
+++ js/loop11.js
@@ -1,4 +1,1 @@
-document.write(<script type="text/javascript">);
   window.loop11_key = "c28db8607cebb40ebbe9cff35e99aeab56a3f3e7";
-document.write(</script>);
-document.write(<script src="//cdn.loop11.com/my/loop11.js" type="text/javascript" async="async"></script>);
